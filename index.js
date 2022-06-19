50a05ce755fb7a1eb59136af493b23c04ffd4393
carsalcedo <fercarrera07@gmail.com>
2022-06-19 19:32:58 -0400

    sin varaiables

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv'
+//import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
 
 const app = express();
-dotenv.config();
+//dotenv.config();
 
 app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
@@ -21,9 +21,11 @@ app.get('/', (req, res) =>{
     res.send('My API is Running')
 })
 
+const CONNECTION_URL = 'mongodb+srv://fercarrera:4s14nsucr3@cluster0.onank.mongodb.net/?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
