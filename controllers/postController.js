31bd9bf86a5b80d7cf0f3f1cd3554cb9d88a49bd
carsalcedo <fercarrera07@gmail.com>
2022-06-28 18:12:51 -0400

    pagination

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,14 +2,32 @@ import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js"
 
 export const getPosts = async (req, res) => {
+    const { page } = req.query;
+    
     try {
-        const postMessages = await PostMessage.find();
-        console.log(postMessages)
+        const LIMIT = 6;
+        const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
+    
+        const total = await PostMessage.countDocuments({});
+        const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
+        console.log(posts)
+        res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT)});
+    } catch (error) {    
+        res.status(404).json({ message: error.message });
+    }
+}
 
-        res.status(200).json(postMessages)
+export const getPostsBySearch = async (req, res) => {
+    const { searchQuery, tags } = req.query;
 
-    } catch (error) {
-        res.status(404).json({ message: error.message})
+    try {
+        const title = new RegExp(searchQuery, "i");
+
+        const posts = await PostMessage.find({ $or: [ { title }, { tags: { $in: tags.split(',') } } ]});
+
+        res.json({ data: posts });
+    } catch (error) {    
+        res.status(404).json({ message: error.message });
     }
 }
 
